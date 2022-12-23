987d863db73b24331f5ea17a162961f0dc56c6fc
fredrikadr <fredrikadr@gmail.com>
2022-12-23 13:48:59 +0100

    Added function to handle multiple choice questions

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 let url = "https://opentdb.com/api.php?";
 
-let numQuestionsNode = document.querySelector("#numQuestions")
+let numQuestionsNode = document.querySelector("#numQuestions");
+let questionNode = document.querySelector("#question")
 
 
 async function getQuestions(url) {
@@ -9,6 +10,57 @@ async function getQuestions(url) {
     `${url}amount=${numQuestionsNode.value}`
   );
   const JSON = await response.json();
-  return JSON;
+  console.log(JSON.results)
+  return JSON.results;
 }
 
+async function startQuiz(questions) {
+  let currentQIndex = 0;
+  let quizArray = await questions;
+  let alternatives = [];
+  let savedAnswers = [];
+  let randomIndex = Math.floor(Math.random() * quizArray[currentQIndex].incorrect_answers.length + 1); // Creates a random index for inserting correct answer
+  
+  
+  questionNode.innerHTML += `<p>${quizArray[currentQIndex].question}</p>`; // Writes the question to the page
+
+
+  if (quizArray[currentQIndex].type == "multiple") { // If question is multiple choice
+
+    quizArray[currentQIndex].incorrect_answers // Pushes incorrect answers to alternatives array
+      .forEach(alternative => alternatives
+        .push(alternative))
+        
+    
+    alternatives
+      .splice(randomIndex, 0, quizArray[currentQIndex].correct_answer) // Places the correct answer at a random index in alternatives array
+    
+    
+    alternatives.forEach(alternative =>
+      questionNode.innerHTML +=`
+        
+        <label for="${alternative}">${alternative}</label>
+        <input type=radio id="${alternative}" name="alternative" value="${alternative}">`)
+      
+    
+    console.log(alternatives)
+
+
+  
+  }
+  
+  else if (quizArray.type = "boolean") { // If question is true or false
+    quizArray[currentQIndex].incorrect_answers
+  }
+
+}
+
+
+
+
+function printBooleanAlternatives(question) {
+
+}
+
+
+startQuiz(getQuestions(url))
