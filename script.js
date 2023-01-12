dd84061904908ca64ee5b0c3f5ddbddc2c568566
fredrikadr <fredrikadr@gmail.com>
2023-01-12 22:23:41 +0100

    added a way to save prev answers

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
 let url = "https://opentdb.com/api.php?";
 
-let numQuestionsNode = document.querySelector("#numQuestions");
-let questionNode = document.querySelector("#question")
-
+const numQuestionsNode = document.querySelector("#numQuestions");
+const questionNode = document.querySelector("#question")
+const buttonContainerNode = document.querySelector("#button-container")
+let currentQIndex = 0;
+let savedAnswers = [];
 
 
 async function getQuestions(url) {
@@ -15,13 +17,19 @@ async function getQuestions(url) {
   return JSON.results;
 }
 
-async function startQuiz(questions, questionIndex, answers) {
-  let currentQIndex = questionIndex
+async function startQuiz(questions) {
   let quizArray = await questions;
   let alternatives = [];
-  let savedAnswers = answers;
+  
   let randomIndex = Math.floor(Math.random() * quizArray[currentQIndex].incorrect_answers.length + 1); // Creates a random index for inserting correct answer
 
+
+  // Pushes empty strings to savedAnswers equal to the number of alternatives
+  if (savedAnswers.length < 1) {
+    for (let i = 0; i < alternatives.length; i++) {
+      savedAnswers.push("");
+    }
+  }
 
   questionNode.innerHTML += `<p>${quizArray[currentQIndex].question}</p>`; // Writes the question to the page
 
@@ -37,12 +45,25 @@ async function startQuiz(questions, questionIndex, answers) {
       .splice(randomIndex, 0, quizArray[currentQIndex].correct_answer) // Places the correct answer at a random index in alternatives array
 
 
-    alternatives.forEach(alternative =>
-      questionNode.innerHTML += `
+    alternatives.forEach(alternative => {
+
+      if (alternative == savedAnswers[currentQIndex]) {
+        questionNode.innerHTML += `
         
-      <input class="radio-btn" type=radio id="${alternative}" name="alternative" value="${alternative}">
+      <input checked class="radio-btn" type=radio id="${alternative}" name="alternative" value="${alternative}">
       <label for="${alternative}">${alternative}</label> <br>
-      `);
+      
+      `;
+      } else {
+        questionNode.innerHTML += `
+          
+        <input class="radio-btn" type=radio id="${alternative}" name="alternative" value="${alternative}">
+        <label for="${alternative}">${alternative}</label> <br>
+        
+        `;
+
+      }
+    })
 
       
     console.log(alternatives)
@@ -50,6 +71,7 @@ async function startQuiz(questions, questionIndex, answers) {
   }
 
   else if (quizArray.type = "boolean") { // If answer is true or false
+    
     questionNode.innerHTML += `
     <input class="radio-btn" type=radio id="true" name="alternative" value="false"}">
       <label for="true">True</label> <br>
@@ -57,10 +79,20 @@ async function startQuiz(questions, questionIndex, answers) {
       <label for="false">False</label> <br>
     `;
   }
+  //Saves answer to current question in array
+  questionNode.addEventListener("click", (event) => {
+    if (event.target === event.currentTarget) {
+      return;
+    } else if (!event.target.value) {
+      return;
+    } else {
+      savedAnswers[currentQIndex] = event.target.value;
+    }
+  });
 
 
   //Buttons
-  questionNode.innerHTML += `
+  buttonContainerNode.innerHTML += `
   <button id="back-btn">Back</button>
   <button id="next-btn">Next</button>
   `;
@@ -68,19 +100,21 @@ async function startQuiz(questions, questionIndex, answers) {
   let nextButtonNode = document.querySelector("#next-btn");
 
   backButtonNode.addEventListener("click", () => {
-    if (currentQIndex == 0) {
+    if (currentQIndex <= 0) {
       return;
     } else {
       currentQIndex--;
       questionNode.innerHTML = "";
-      startQuiz(quizArray, currentQIndex);
+      buttonContainerNode.innerHTML = "";
+      startQuiz(quizArray, currentQIndex, savedAnswers);
     }
   })
 
   nextButtonNode.addEventListener("click", () => {
     currentQIndex++;
     questionNode.innerHTML = "";
-    startQuiz(quizArray, currentQIndex);
+    buttonContainerNode.innerHTML = "";
+    startQuiz(quizArray, currentQIndex, savedAnswers);
   })
 
 }
